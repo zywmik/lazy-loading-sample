acbe30b9b2b3d965a4519d7928518c1576b5e3f9
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2020-02-03 14:01:28 +0100

    img fix

--- src/mysterious-image.js
+++ src/mysterious-image.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import CatTheTeacher from './img/cat-the-teacher.jpg'
+import React from "react";
+import CatTheTeacher from "./img/cat-the-teacher.jpg";
 
 const MysteriousImage = () => {
     return (
         <div>
             <h1>Hello there my student!</h1>
             <img
-                src={CatTheTeacher}
-                alt="Cat the teacher"
+              src={CatTheTeacher}
+              alt="Cat the teacher"
+              height="200px"
             />
         </div>
-    )
+    );
 };
 
 export default MysteriousImage;
